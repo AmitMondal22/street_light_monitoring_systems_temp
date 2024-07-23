694ebcf35075c7ec7504d6b25ea256c89709a03f
Amit Mondal <amit.ndk3@gmail.com>
2024-07-23 16:14:32 +0530

    add encode data

--- controllers/api/LoraApi.py
+++ controllers/api/LoraApi.py
@@ -46,48 +46,15 @@ def send_downlink(user: Any, params: Any) -> dict:
     
 @staticmethod
 async def webhooks_send_downlink():
-    # try:
-        # payload = {
-        #     'downlinks': [
-        #         {
-        #             'frm_payload': '*hello#',
-        #             'f_port': 15,
-        #             'priority': 'HIGHEST'
-        #             # 'priority': 'NORMAL'
-        #         }
-        #     ]
-        # }
-        
-        # headers = {
-        #     'Content-Type': 'application/json',
-        #     'Authorization': 'Bearer NNSXS.SKSGTTX6IIDKM7THS3RATJBRL5ZHMKO4A6ZBGXY.WVF3AVQVGOAVWK3E7CIGPVLXPHREIL5D5FXJCK5E2BCKZWL6PAVA',
-        # }
-        
-        # url = 'https://eu1.cloud.thethings.network/api/v3/as/applications/streetlighttechavo/webhooks/test/devices/eui-0080e115002b5637/down/replace'
-        
-        # # Perform a synchronous POST request
-        # response = requests.post(url, headers=headers, json=payload)
-        
-        # # Check if the request was successful
-        # # response.raise_for_status()
-        # print("///////////////")
-        # print(jsonable_encoder(response))
-
-        # try:
-        #     return {"status":"success"}  # Return the JSON response
-        # except ValueError:
-        #     raise HTTPException(status_code=500, detail="Invalid JSON response from the server")
-
-    # except requests.exceptions.RequestException as e:
-    #     # Handle network errors or bad responses
-    #     raise HTTPException(status_code=500, detail=f"Request failed: {str(e)}")
+   
+    frm_payload="hello world"
     time.sleep(1)
     url = "https://eu1.cloud.thethings.network/api/v3/as/applications/streetlighttechavo/webhooks/test/devices/eui-0080e115002b5637/down/replace"
 
     payload = json.dumps({
     "downlinks": [
         {
-        "frm_payload": "aGVsbG8=",
+        "frm_payload": frm_payload.encode('utf-8'),
         "f_port": 15,
         "priority": "NORMAL"
         }
