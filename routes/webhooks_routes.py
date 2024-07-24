935087581712be267dbf55f86999c1ad3be0655b
Amit Mondal <amit.ndk3@gmail.com>
2024-07-24 13:14:33 +0530

    add print

--- routes/webhooks_routes.py
+++ routes/webhooks_routes.py
@@ -15,6 +15,7 @@ async def testing(request: Request):
     # try:
         data = await request.json()
         print(data)
+        print("////////////////////////////'''''''''''''''''''''''''''")
         frm_payload_base64 = data["uplink_message"]["frm_payload"]
         frm_payload_bytes = base64.b64decode(frm_payload_base64)
         data_str = frm_payload_bytes.decode('utf-8')
