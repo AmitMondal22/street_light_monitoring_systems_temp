dcad55e00e5e143b2de4cfb7f88fc4e814cebc80
Amit Mondal <amit.ndk3@gmail.com>
2024-08-07 11:02:46 +0530

    edfcew

--- controllers/api/LoraApi.py
+++ controllers/api/LoraApi.py
@@ -112,9 +112,10 @@ async def webhooks_send_downlink_test(dev_eui: str, payload: str):
     # }
     data = {
         "confirmed": False,  # Whether to require confirmation of receipt
-        "f_port": 1,  # Use a valid f_port value (> 0)
-        "data": payload  # Base64 encoded payload
+        "f_port": 1,         # Use a valid f_port value (> 0)
+        "data": payload      # Base64 encoded payload
     }
+
     print(payload)
     headers = {
         "Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiYTBmOTUzZTQtNWRlMi00NDhiLWJiMmQtYWQxOTM3OTMxMGRlIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTcyMjk0NDE5Miwic3ViIjoiYXBpX2tleSJ9.ep4D5-YaGQru0o0ur77TK5CuwtFFNPlQaSu0zfrw6Lo",
